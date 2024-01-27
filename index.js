9494763ba78e87735ec8a83c3552e8c3c49b1d0e
judy_her <judith85dm@ucla.edu>
2024-01-27 14:24:18 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const inquirer = require('inquirer');
+const { writeFile } = require('fs').promises;
+
+const questions = () => {
+  return inquirer.prompt([
+    {
+      name: 'options',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'view all departments',
+        'view all departments',
+        'view all roles',
+        'view all employees',
+        'add a department',
+        'add a role',
+        'add an employee',
+        'update an employee role',
+      ],
+    },
+  ]);
+};
+
+const init = () => {
+  questions();
+  // .then((answers) => writeFile('questions.txt', questions(answers)))
+  // .then(() => console.log('Text File succesfully written'))
+  // .catch((err) => console.log(err));
+};
+
+init();

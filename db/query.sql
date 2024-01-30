2de44e357661c4a92808e7694f87a2673fba38d5
judy_her <judith85dm@ucla.edu>
2024-01-29 16:19:51 -0800

    added manager_id per employess and updated query with that manager name and last name

--- db/query.sql
+++ db/query.sql
@@ -2,9 +2,20 @@ SELECT roles.id, roles.title, roles.salary, department.department_name
 FROM roles
 JOIN department ON roles.department_id = department.id;
 
-
-
-SELECT employee.id, employee.first_name, employee.last_name, roles.title as role_title, department.department_name, roles.salary
+SELECT employee.id, employee.first_name, employee.last_name, roles.title as role_title, department.department_name, roles.salary, employee.manager_id 
 FROM employee 
 JOIN roles ON employee.role_id = roles.id
 JOIN department ON roles.department_id = department.id;
+
+-- SELECT e1.id, e1.first_name, e1.last_name, e1.role_id, e2.first_name AS manager_first_name, e2.last_name AS manager_last_name
+-- FROM employee AS e1
+-- LEFT JOIN employee AS e2 ON e1.manager_id = e2.id;
+
+SELECT e1.id, e1.first_name, e1.last_name, roles.title AS role_title, department.department_name, roles.salary, e2.first_name AS manager_first_name, e2.last_name AS manager_last_name
+FROM employee AS e1
+JOIN roles ON e1.role_id = roles.id
+JOIN department ON roles.department_id = department.id
+LEFT JOIN (
+    SELECT id, first_name, last_name
+    FROM employee
+) AS e2 ON e1.manager_id = e2.id;
--- db/seeds.sql
+++ db/seeds.sql
@@ -13,21 +13,41 @@ VALUES (1,"Salesperson", 40025, 1),
     (5,"Accountant", 70025, 3 ),
     (6,"Accountant Manager", 90156, 3),
     (7,"Software Engineer", 75025, 4 ),
-    (8,"Lead Software Engineer", 90125, 4),
-    (9,"CEO", 99125, 5);
-
+    (8,"Lead Software Engineer", 90125, 4);
+   
 
 INSERT INTO employee(first_name, last_name, role_id)
 VALUES ("Alice", "Turner", 1),
        ("Bob", "Smith", 1),
-       ("Derrick", "Mitchell", 2),
+       ("Derreck", "Mitchell", 2),
        ("Sophie", "Reynolds", 3),
        ("Emma", "Johnson", 4),
        ("Benjamin", "Clark", 5),
        ("Olivia", "Martinez", 6),
        ("Liam", "Thompson", 7),
-       ("Ava", "Walker", 8),
-       ("Sophia", "Wright", 9);
+       ("Sophia", "Wright", 7),
+       ("Ava", "Walker", 8);
+     
+UPDATE employee AS e1
+JOIN employee AS e2 ON e2.role_id = 2
+SET e1.manager_id = e2.id
+WHERE e1.role_id = 1;
+
+UPDATE employee AS e1
+JOIN employee AS e2 ON e2.role_id = 8
+SET e1.manager_id = e2.id
+WHERE e1.role_id = 7;
+
+UPDATE employee AS e1
+JOIN employee AS e2 ON e2.role_id = 6
+SET e1.manager_id = e2.id
+WHERE e1.role_id = 5;
+
+UPDATE employee AS e1
+JOIN employee AS e2 ON e2.role_id = 4
+SET e1.manager_id = e2.id
+WHERE e1.role_id = 3;
+
 
 SELECT * FROM department;
 SELECT * FROM roles;

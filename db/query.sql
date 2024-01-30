d65e1f40f4b2baed9e3bdaabb8a642e1eb6111d1
judy_her <judith85dm@ucla.edu>
2024-01-30 15:14:38 -0800

    added code to query where manager name prints instead of number

--- db/query.sql
+++ db/query.sql
@@ -2,7 +2,7 @@ SELECT roles.id, roles.title, roles.salary, department.department_name
 FROM roles
 JOIN department ON roles.department_id = department.id;
 
---NOTE this needs fixing
+-- NOTE this needs fixing
 -- SELECT employee.id, 
 -- employee.first_name, 
 -- employee.last_name, 
@@ -16,3 +16,16 @@ JOIN department ON roles.department_id = department.id;
 -- JOIN department ON roles.department_id = department.id
 -- LEFT JOIN employee ON employee.manager_id = manager_id;
 
+-- code from tutor salvarado in AskBCS
+SELECT 
+    e.first_name AS Employee_First_Name,
+    e.last_name AS Employee_Last_Name,
+    r.title AS Role,
+    d.department_name AS Department,
+    m.first_name AS Manager_First_Name,
+    m.last_name AS Manager_Last_Name
+FROM
+    employee e
+LEFT JOIN roles r ON e.role_id = r.id
+LEFT JOIN department d ON r.department_id = d.id
+LEFT JOIN employee m ON e.manager_id = m.id;

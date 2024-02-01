6ce5f8cc4f372f339907ba8ac9b388412fa54f91
judy_her <judith85dm@ucla.edu>
2024-01-31 17:58:31 -0800

    added async to main and view all dept functions, displays tables and prompt question after it

--- server.js
+++ server.js
@@ -11,59 +11,61 @@ const db = mysql.createConnection(
   console.log(`Connected to company_db database`)
 );
 
-//function to view all departments
-const viewAll_Departments = () => {
-  const sql = `SELECT id, department_name AS title FROM department`;
+const viewAll_Departments = async () => {
+  return new Promise((resolve, reject) => {
+    const sql = `SELECT id, department_name AS title FROM department`;
 
-  db.query(sql, (err, rows) => {
-    if (err) {
-      console.error(err);
-      db.end();
-      return;
-    }
-    console.table(rows);
-    db.end();
+    db.query(sql, (err, rows) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.table(rows);
+        resolve();
+      }
+    });
   });
 };
 
 //main menu
-const startQuestions = () => {
-  const questions = [
-    {
-      name: 'option',
-      type: 'list',
-      message: 'What would you like to do?',
-      choices: [
-        'view all departments',
-        'view all roles',
-        'view all employees',
-        'add a department',
-        'add a role',
-        'add an employee',
-        'update an employee role',
-        'exit',
-      ],
-    },
-  ];
+const startQuestions = async () => {
+  let exit = false;
 
-  const askQuestions = () => {
-    inquirer.prompt(questions).then((answers) => {
+  while (!exit) {
+    const questions = [
+      {
+        name: 'option',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'view all departments',
+          'view all roles',
+          'view all employees',
+          'add a department',
+          'add a role',
+          'add an employee',
+          'update an employee role',
+          'exit',
+        ],
+        pageSize: 10,
+      },
+    ];
+
+    await inquirer.prompt(questions).then(async (answers) => {
       const { option } = answers;
 
       switch (option) {
         case 'view all departments':
-          viewAll_Departments();
-          askQuestions();
+          await viewAll_Departments();
           break;
         case 'exit':
           console.log('Goodbye!');
+          exit = true;
           db.end();
           break;
-        // case  'view all roles':
       }
     });
-  };
-  askQuestions();
+  }
 };
 
 //start application
